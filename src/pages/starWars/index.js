4a399c5a0965f996f508e75cd393f50dc17a8d31
Yan Martins Esteves <yan.esteves@audaces.com>
2022-08-25 18:22:59 -0300

    adicionando propriedade tipo nos personagens star wars

--- src/pages/starWars/index.js
+++ src/pages/starWars/index.js
@@ -10,32 +10,38 @@ export default function StarWars() {
         {
             id: 1,
             nome: 'Luke Skywalker',
-            imagem: 'https://starwars-visualguide.com/assets/img/characters/1.jpg'
+            imagem: 'https://starwars-visualguide.com/assets/img/characters/1.jpg',
+            tipo: 'humano'
         },
         {
             id: 2,
             nome: 'C-3PO',
-            imagem: 'https://starwars-visualguide.com/assets/img/characters/2.jpg'
+            imagem: 'https://starwars-visualguide.com/assets/img/characters/2.jpg',
+            tipo: 'robo'
         },
         {
             id: 3,
             nome: 'Darth Vader',
-            imagem: 'https://starwars-visualguide.com/assets/img/characters/4.jpg'
+            imagem: 'https://starwars-visualguide.com/assets/img/characters/4.jpg',
+            tipo: 'humano'
         },
         {
             id: 4,
             nome: 'Leia Organa',
-            imagem: 'https://starwars-visualguide.com/assets/img/characters/5.jpg'
+            imagem: 'https://starwars-visualguide.com/assets/img/characters/5.jpg',
+            tipo: 'humano'
         },
         {
             id: 5,
             nome: 'R2-D2',
-            imagem: 'https://starwars-visualguide.com/assets/img/characters/3.jpg'
+            imagem: 'https://starwars-visualguide.com/assets/img/characters/3.jpg',
+            tipo: 'robo'
         },
         {
             id: 6,
             nome: 'Obi-Wan Kenobi',
-            imagem: 'https://starwars-visualguide.com/assets/img/characters/10.jpg'
+            imagem: 'https://starwars-visualguide.com/assets/img/characters/10.jpg',
+            tipo: 'humano'
         }
     ]
 
