dfe424ccf2eb55c612778e4c05c91d9dd3ac8344
Yan Martins Esteves <yan.esteves@audaces.com>
2022-08-26 19:45:09 -0300

    exemplo useRef e comparação com variável local

--- src/pages/contagemRef/index.js
+++ src/pages/contagemRef/index.js
@@ -1,17 +1,23 @@
 import { useRef } from 'react';
 
 export default function ContagemRef() {
-    
-    const countRef = useRef(0);
 
-    let value = 0;
+  const countRef = useRef(0);
+  let value = 0;
 
-    const contar = () => {
-      countRef.current ++;
-      value += 1;
-      console.log(`Ref: ${countRef.current}`);
-      console.log(`Value: ${value}`)
-    };
+  function contar() {
+    countRef.current++;
+    value += 1;
+    console.log(`Ref: ${countRef.current}`);
+    console.log(`Value: ${value}`)
+  };
 
-    return <button onClick={contar}>Clicar!</button>;      
+  return (
+    <div style={{'display': 'flex', 'flexDirection': 'column'}}>
+      <label>REF: {countRef.current}</label>
+      <label>Value: {value}</label>
+      <button onClick={contar}>Salvar</button>
+
+    </div>
+  );
 }
