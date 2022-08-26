80a1b989115f70a85fed13057fe5fcc28702b529
Yan Martins Esteves <yan.esteves@audaces.com>
2022-08-25 21:08:39 -0300

    função de parada para contador

--- src/pages/cronometro/index.js
+++ src/pages/cronometro/index.js
@@ -1,20 +1,25 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Cronometro() {
 
     const [timer, setTimer]= useState(0)
 
+    const meuContadorId = useRef();
+
     function iniciarContagem() {
-        setInterval(() => {
+        meuContadorId.current = setInterval(() => {
             setTimer(prev => prev + 1)
-        }, 200);
+        }, 1000);
     }
 
+    function pararContagem() {
+        clearInterval(meuContadorId.current)
+    }
     return (
         <div style={{'display': 'block', 'width': '240px','margin': '0 auto', 'textAlign': 'center', 'transform': 'scale(2) translateY(50%)'}}>
             <h2>{timer}</h2>
             <button onClick={iniciarContagem}>Iniciar</button>
-            <button>Parar</button>
+            <button onClick={pararContagem}>Parar</button>
         </div>
     );
 }
